89dfbfc24e3d50ee3b466ed38fc13b9bae05ddd9
James Hager <james@avantiapplications.com>
2022-02-22 15:01:16 -0700

    Clean and add comments

--- movieList/test.test.js
+++ movieList/test.test.js
@@ -13,6 +13,7 @@ afterAll(async () => {
 })
 
 test('delete a movie', async () => {
+    // make a movie
     const movieTitle = 'Tenet'
     const inputField = await driver.findElement(By.xpath('//input'))
     await inputField.sendKeys('Tenet')
@@ -21,67 +22,54 @@ test('delete a movie', async () => {
     await movieButton.click()
     await (await driver).sleep(2000)
 
+    // delete the movie
     const deleteButton = await driver.findElement(By.id(movieTitle))
     await deleteButton.click()
     await (await driver).sleep(2000)
 
+    // confirm that there aren't any movies
     const ulInnerHtml = await driver.findElement(By.xpath('//ul')).getAttribute('innerHTML')
 
     expect(ulInnerHtml.length).toBe(0)
 })
 
 test('cross off a movie', async () => {
+    // make a movie
     const movieTitle = 'Tenet'
     const inputField = await driver.findElement(By.xpath('//input'))
-    await inputField.sendKeys('Tenet')
+    await inputField.sendKeys(movieTitle)
     await (await driver).sleep(2000)
     const movieButton = await driver.findElement(By.css('button'))
     await movieButton.click()
     await (await driver).sleep(2000)
 
+    // cross off the movie
     const movieSpan = await driver.findElement(By.xpath('//ul/li/span'))
     await movieSpan.click()
     await (await driver).sleep(2000)
 
+    // check the class on the movie title span
     const movieClass = await movieSpan.getAttribute("class")
 
     expect(movieClass).toBe('checked')
 })
 
 test('delete a movie and check notification', async () => {
+    // make a movie
     const movieTitle = 'Tenet'
     const inputField = await driver.findElement(By.xpath('//input'))
-    await inputField.sendKeys('Tenet')
+    await inputField.sendKeys(movieTitle)
     await (await driver).sleep(2000)
     const movieButton = await driver.findElement(By.css('button'))
     await movieButton.click()
     await (await driver).sleep(2000)
 
-    const deleteButton = await driver.findElement(By.id(movieTitle))
-    await deleteButton.click()
+    // delete the movie
+    await driver.findElement(By.id(movieTitle)).click()
     await (await driver).sleep(2000)
 
-    const asideElem = await driver.findElement(By.xpath('//aside'))
-    const asideClass = await asideElem.getAttribute("class")
-
-    expect(asideClass).toBe(`hide`)
-})
-
-test('delete a movie and check notification', async () => {
-    const movieTitle = 'Tenet'
-    const inputField = await driver.findElement(By.xpath('//input'))
-    await inputField.sendKeys('Tenet')
-    await (await driver).sleep(2000)
-    const movieButton = await driver.findElement(By.css('button'))
-    await movieButton.click()
-    await (await driver).sleep(2000)
-
-    const deleteButton = await driver.findElement(By.id(movieTitle))
-    await deleteButton.click()
-    await (await driver).sleep(2000)
-
-    const asideElem = await driver.findElement(By.xpath('//aside'))
-    const asideClass = await asideElem.getAttribute("class")
+    // check the notification
+    const asideClass = await driver.findElement(By.xpath('//aside')).getAttribute("class")
 
     expect(asideClass).toBe(`hide`)
 })
